49b970229b957eb4fd5630c7f84161d073f76c71
yuehan2710 <133289544+yuehan2710@users.noreply.github.com>
2025-03-08 12:58:23 -0800

    Update connect_to_eth.py

--- connect_to_eth.py
+++ connect_to_eth.py
@@ -14,8 +14,8 @@ def connect_to_eth():
 	url = "https://bsc-testnet-rpc.publicnode.com"  # FILL THIS IN
 	w3 = Web3(HTTPProvider(url))
 	assert w3.is_connected(), f"Failed to connect to provider at {url}"
-
 	return w3
+
 
 def connect_with_middleware(contract_json):
 	with open(contract_json, "r") as f:
@@ -24,32 +24,23 @@ def connect_with_middleware(contract_json):
 		address = d['address']
 		abi = d['abi']
 
-	
 	# TODO complete this method
 	# The first section will be the same as "connect_to_eth()" but with a BNB url
-	
 	bsc_url = "https://bsc-testnet-rpc.publicnode.com"
-	
 	w3 = Web3(HTTPProvider(bsc_url))
 
-
 	assert w3.is_connected(), f"Failed to connect to provider at {bsc_url}"
-	print("Successfully connected to Binance Smart Chain!")
-
-	# Inject PoA middleware (required for BSC)
-    	w3.middleware_onion.inject(ExtraDataToPOAMiddleware, layer=0)
-
 	
 
 	# The second section requires you to inject middleware into your w3 object and
 	# create a contract object. Read more on the docs pages at https://web3py.readthedocs.io/en/stable/middleware.html
 	# and https://web3py.readthedocs.io/en/stable/web3.contract.html
 	
-	
+	#Inject middleware for Proof-of-Authority (PoA) chains (required for BSC)
+	w3.middleware_onion.inject(ExtraDataToPOAMiddleware, layer = 0)
 
 	#Create contract object
 	contract = w3.eth.contract(address=Web3.to_checksum_address(address), abi=abi)
-	print(f" Connected to contract at {contract.address}")
 
 	return w3, contract
 
