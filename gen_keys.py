dc06676f6f1f322bed4e081ec8ed83065e986dda
yuehan2710 <133289544+yuehan2710@users.noreply.github.com>
2025-03-23 10:39:14 -0700

    Update gen_keys.py

--- gen_keys.py
+++ gen_keys.py
@@ -16,6 +16,8 @@ def sign_message(challenge, filename="secret_key.txt"):
         key = f.readlines()
     assert(len(key) > 0), "Your account secret_key.txt is empty"
 
+    private_key = key[0].strip()
+
     w3 = Web3()
     message = encode_defunct(challenge)
 
