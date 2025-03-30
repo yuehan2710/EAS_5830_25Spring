fa9423344c009d87463ae4bff1f5006c8c6833aa
yuehan2710 <133289544+yuehan2710@users.noreply.github.com>
2025-03-29 23:10:54 -0700

    Update submitProof.py

--- submitProof.py
+++ submitProof.py
@@ -180,7 +180,7 @@ def send_signed_msg(proof, random_leaf):
     })
     
     # Sign and send
-    signed_tx = w3.eth.account.sign_transaction(tx, acct.key)
+    signed_tx = acct.sign_transaction(tx)
     tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
     
     return tx_hash.hex()
